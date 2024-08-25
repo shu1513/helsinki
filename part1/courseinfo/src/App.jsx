0ce51a320e0109758355fca8dc45595e78e7b6af
Shu Yang <shu@Shus-MBP.lan>
2024-08-25 13:10:45 -0700

    exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,20 +3,23 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
+const Part = (props) => {
+  console.log("running part component");
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  );
+};
+
 const Content = (props) => {
   console.log("content component");
   return (
-    <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+      <Part part={props.part3} exercise={props.exercises3} />
+    </div>
   );
 };
 
@@ -44,15 +47,10 @@ const App = () => {
       <Header course={course} />
       <Content
         part1={part1}
+        exercises1={exercises1}
         part2={part2}
+        exercises2={exercises2}
         part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
         exercises3={exercises3}
       />
     </div>
