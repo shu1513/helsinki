68175c1872cc6cbb327957b09956f89dc20d0645
Shu Yang <shu@Shus-MBP.lan>
2024-09-02 21:44:30 -0700

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,12 +10,21 @@ const App = () => {
     <div>
       <h3>Give feedback</h3>
       <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="bad" />
-      <Button handleClick={() => setBad(bad + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <h3>statistics</h3>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>
+        average
+        {good + neutral + bad ? (good - bad) / (good + neutral + bad) : 0}
+      </div>
+      <div>
+        positive{" "}
+        {good + neutral + bad ? (good / (good + neutral + bad)) * 100 : 0} %
+      </div>
     </div>
   );
 };
