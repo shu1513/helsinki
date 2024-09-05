818ae8ed385278589cdadbc166ef5caccf6bb71b
Shu Yang <shu@Shus-MBP.lan>
2024-09-04 22:52:54 -0700

    1.13

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const points = Array(anecdotes.length).fill(0);
+  const [votesArray, setVotesArray] = useState(points);
+
+  const vote = (points, index) => {
+    const copy = [...points];
+    //add the points of the vote
+    copy[index] += 1;
+    //reset the array and render the page
+    setVotesArray(copy);
+  };
+
+  return (
+    <>
+      <div>{anecdotes[selected]}</div>
+      <div>has {votesArray[selected]} votes</div>
+      <Button text="vote" handleOnClick={() => vote(votesArray, selected)} />
+      <Button
+        text={"next anecdote"}
+        handleOnClick={() =>
+          setSelected(Math.floor(Math.random() * anecdotes.length))
+        }
+      />
+    </>
+  );
+};
+
+const Button = ({ text, handleOnClick }) => {
+  return <button onClick={handleOnClick}>{text}</button>;
+};
+
+export default App;
