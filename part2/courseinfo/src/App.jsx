9f0b5c0083d20e4eda0523bef31391ed80f5e683
Shu Yang <shu@Shus-MBP.lan>
2024-09-07 12:34:37 -0700

    2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,56 @@
+const Header = ({ courseName }) => <h3>{courseName}</h3>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+
+const Course = ({ course }) => {
+  console.log("Course works for return before map");
+  return (
+    <>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+const App = () => {
+  console.log("app is loaded");
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
