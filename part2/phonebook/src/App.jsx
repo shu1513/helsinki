f4c9d65eeb1d0166882fa3d42feb738869e2960b
Shu Yang <shu@Shus-MBP.lan>
2024-09-09 21:13:57 -0700

    2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const addName = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name:{" "}
+          <input
+            placeholder="enter name here"
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index) => (
+        <li key={index}>{person.name}</li>
+      ))}
+    </div>
+  );
+};
+
+export default App;
