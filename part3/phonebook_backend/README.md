38da988fa770341dda964620e1b46bc8444b29d8
Shu Yang <shu@Shus-MBP.lan>
2024-09-28 22:11:01 -0700

    3.11

---
+++ part3/phonebook_backend/README.md
@@ -0,0 +1,1 @@
+[https://phonebook-backend-shu-yang.fly.dev/api/persons]
--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+app.use(express.static("dist"));
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
@@ -77,7 +78,7 @@ app.post("/api/persons", (request, response) => {
   console.log(request.body);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
