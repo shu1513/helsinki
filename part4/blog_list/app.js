5181eba8f21560de624c3f946dc7f405c0fe75df
Shu Yang <shu@Shus-MBP.lan>
2024-10-11 22:56:40 -0700

    4.2

---
+++ part4/blog_list/app.js
@@ -0,0 +1,26 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+    console.log(mongoose.connection.db.databaseName);
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
