e0542033991694921f94f4b3518dc9f3f8c677b5
Shu Yang <shu@Shus-MBP.lan>
2024-10-12 19:55:01 -0700

    4.5

--- part4/blog_list/tests/favoriteBlog.test.js
+++ part4/blog_list/tests/favoriteBlog.test.js
@@ -66,10 +66,17 @@ const blogs = [
 
 describe("favorite", () => {
   test("with one blog", () => {
-    assert.deepStrictEqual(favoriteBlog(listWithOneBlog).likes, 5);
+    assert.deepStrictEqual(favoriteBlog(listWithOneBlog), listWithOneBlog[0]);
   });
 
   test("with mutiple blogs", () => {
-    assert.deepStrictEqual(favoriteBlog(blogs).likes, 12);
+    assert.deepStrictEqual(favoriteBlog(blogs), {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    });
   });
 });
