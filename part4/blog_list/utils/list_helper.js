20f3c044595fb5f051e4ccdc0ad317670f2b0d5f
Shu Yang <shu@Shus-MBP.lan>
2024-10-12 23:15:59 -0700

    4.6

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -16,7 +16,29 @@ const favoriteBlog = (blogsArray) => {
   return blogsArray.reduce(reducer);
 };
 
+const mostBlogs = (blogsArray) => {
+  let publications = {};
+  for (let i = 0; i < blogsArray.length; i++) {
+    const author = blogsArray[i].author;
+    if (publications[author]) {
+      publications[author] += 1;
+    } else {
+      publications[author] = 1;
+    }
+  }
+  let maxPublication = 0;
+  let mostPublishedAuthor;
+  for (author in publications) {
+    if (publications[author] > maxPublication) {
+      maxPublication = publications[author];
+      mostPublishedAuthor = { author: author, blogs: publications[author] };
+    }
+  }
+  return mostPublishedAuthor;
+};
+
 module.exports = {
+  mostBlogs,
   favoriteBlog,
   totalLikes,
   dummy,
