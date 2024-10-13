5b8ca94f16e6c815c1013fcefb9c7f37a5263867
Shu Yang <shu@Shus-MBP.lan>
2024-10-12 19:44:40 -0700

    4.5

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -9,7 +9,15 @@ const totalLikes = (blogsArray) => {
   return blogsArray.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogsArray) => {
+  const reducer = (maxLikedBlog, currentblog) => {
+    return maxLikedBlog.likes >= currentblog.likes ? maxLikedBlog : currentblog;
+  };
+  return blogsArray.reduce(reducer);
+};
+
 module.exports = {
+  favoriteBlog,
   totalLikes,
   dummy,
 };
