ab214bf909892bde00fa4ecb7d829c7a0befa51b
Shu Yang <shu@Shus-MBP.lan>
2024-10-16 14:01:56 -0700

    4.9

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -8,6 +8,13 @@ blogsRouter.get("/", (request, response) => {
   });
 });
 
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else response.status(404).end();
+});
+
 blogsRouter.post("/", (request, response) => {
   const blog = new Blog(request.body);
 
--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -16,11 +16,29 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test.only("blogs are returned as json", async () => {
+test("blogs are returned as json", async () => {
   const response = await api.get("/api/blogs");
   assert.strictEqual(response.statusCode, 200);
   assert.match(response.headers["content-type"], /application\/json/);
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
+});
+
+test("unique identifier is named id with existing blog", async () => {
+  const response = await api.get("/api/blogs/5a422a851b54a676234d17f7");
+  assert.strictEqual(response.statusCode, 200);
+  assert.match(response.headers["content-type"], /application\/json/);
+  assert.deepStrictEqual(response.body, {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0,
+  });
+});
+
+test("unique identifier is named id with non-existing blog", async () => {
+  await api.get("/api/blogs/aa422a851b54a676234d17f7").expect(404);
 });
 
 after(async () => {
