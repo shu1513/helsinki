6724db6f30e000b8b1fb7b2be18fb568d7982f95
Shu Yang <shu@Shus-MBP.lan>
2024-10-27 13:14:48 -0700

    4.18

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -4,9 +4,14 @@ const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -27,6 +32,8 @@ app.use(middleware.requestLogger);
 app.use("/api/blogs", blogsRouter);
 
 app.use("/api/users", usersRouter);
+
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 
