c152bac2de794af20c92a3ab600df4ed60e763d7
Shu Yang <shu@Shus-MBP.lan>
2024-11-14 13:40:56 -0800

    5.20

--- part5/blogs_frontend/testing_playwright_bloglist/tests/blog_app.spec.js
+++ part5/blogs_frontend/testing_playwright_bloglist/tests/blog_app.spec.js
@@ -1,9 +1,10 @@
 const { test, expect, describe, beforeEach } = require("@playwright/test");
+const { loginWith, createBlog } = require("./helper");
 
 describe("Blog app", () => {
   beforeEach(async ({ page, request }) => {
-    await request.post("http://localhost:3003/api/testing/reset");
-    await request.post("http://localhost:3003/api/users", {
+    await request.post("api/testing/reset");
+    await request.post("api/users", {
       data: {
         name: "Matti Luukkainen",
         username: "mluukkai",
@@ -11,7 +12,7 @@ describe("Blog app", () => {
       },
     });
 
-    await page.goto("http://localhost:5173");
+    await page.goto("/");
   });
   test("front page can be opened", async ({ page }) => {
     const locator = await page.getByText("log in to application");
@@ -25,19 +26,55 @@ describe("Blog app", () => {
   });
   describe("login", () => {
     test("succeeds with correct credentials", async ({ page }) => {
-      await page.getByTestId("username").fill("mluukkai");
-      await page.getByTestId("password").fill("salainen");
-      await page.getByRole("button", { name: "login" }).click();
+      await loginWith(page, "mluukkai", "salainen");
       await expect(
         page.getByText("Matti Luukkainen is logged in")
       ).toBeVisible();
     });
     test("fails with wrong credentials", async ({ page }) => {
-      await page.getByTestId("username").fill("mluukkai");
-      await page.getByTestId("password").fill("wrong");
-      await page.getByRole("button", { name: "login" }).click();
+      await loginWith(page, "mluukkai", "wrong password");
 
-      await expect(page.getByText("wrong credentials")).toBeVisible();
+      const errorDiv = await page.locator(".error");
+      await expect(errorDiv).toContainText("Wrong credentials");
+      await expect(errorDiv).toHaveCSS("border-style", "solid");
+      await expect(errorDiv).toHaveCSS("color", "rgb(255, 0, 0)");
+      await expect(
+        page.getByText("Matti Luukkainen is logged in")
+      ).not.toBeVisible();
+    });
+    describe("When logged in", () => {
+      beforeEach(async ({ page }) => {
+        await loginWith(page, "mluukkai", "salainen");
+      });
+      test("a new blog can be created", async ({ page }) => {
+        await createBlog(page, "test title", "test author", "test url");
+        await expect(
+          page.getByText(`a new blog test title by test author added`)
+        ).toBeVisible();
+        await expect(
+          page.locator(`.briefDescription:has-text("test title test author")`)
+        ).toBeVisible();
+      });
+      describe("when logged in with exiting notes", () => {
+        beforeEach(async ({ page }) => {
+          await createBlog(page, "test title", "test author", "test url");
+          await createBlog(page, "test title 2", "test author 2", "test url 2");
+        });
+        test.only("a blog can be liked", async ({ page }) => {
+          const testBlog1 = await page.locator(
+            `.briefDescription:has-text("test title test author")`
+          );
+          await testBlog1.getByText("view").click();
+          const testBlog1Full = page.locator(
+            `.fullDescription:has-text("test title test author")`
+          );
+          await expect(testBlog1Full).toBeVisible();
+          await expect(testBlog1Full.getByText("likes 0 ")).toBeVisible();
+          await testBlog1Full.getByRole("button", { name: "like" }).click();
+          await expect(testBlog1Full.getByText("likes 0 ")).not.toBeVisible();
+          await expect(testBlog1Full.getByText("likes 1 ")).toBeVisible();
+        });
+      });
     });
   });
 });
---
+++ part5/blogs_frontend/testing_playwright_bloglist/tests/helper.js
@@ -0,0 +1,19 @@
+const loginWith = async (page, username, password) => {
+  await page.getByTestId("username").fill(username);
+  await page.getByTestId("password").fill(password);
+  await page.getByRole("button", { name: "login" }).click();
+};
+
+const createBlog = async (page, title, author, url) => {
+  await page.getByRole("button", { name: "new blog" }).click();
+  await page.getByTestId("title").fill(title);
+  await page.getByTestId("author").fill(author);
+  await page.getByTestId("url").fill(url);
+  await page.getByRole("button", { name: "create" }).click();
+  const blogLocator = page.locator(
+    `.briefDescription:has-text("${title} ${author}")`
+  );
+  await blogLocator.waitFor({ state: "visible" });
+};
+
+export { loginWith, createBlog };
