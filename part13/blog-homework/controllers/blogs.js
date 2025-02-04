b8ff122899b557f1d493b06b04b0fe9b63c89cbf
Shu Yang <shu@MacBookPro.lan>
2025-02-03 21:35:50 -0800

    13.7

--- part13/blog-homework/controllers/blogs.js
+++ part13/blog-homework/controllers/blogs.js
@@ -7,6 +7,24 @@ const blogFinder = async (req, _res, next) => {
   next();
 };
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "SequelizeValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  if (error.name === "SequelizeUniqueConstraintError") {
+    return response.status(400).json({ error: "Duplicate entry" });
+  }
+
+  if (error.name === "TypeError") {
+    return response.status(400).json({ error: "Invalid data type" });
+  }
+
+  response.status(500).json({ error: "Internal server error" });
+};
+
 router.get("/", async (req, res) => {
   const blogs = await Blog.findAll();
   console.log(JSON.stringify(blogs));
@@ -14,9 +32,13 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  console.log(req.body);
-  const blog = await Blog.create(req.body);
-  res.json(blog);
+  try {
+    console.log(req.body);
+    const blog = await Blog.create(req.body);
+    res.json(blog);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:id", blogFinder, async (req, res) => {
@@ -38,12 +60,26 @@ router.delete("/:id", blogFinder, async (req, res) => {
 });
 
 router.put("/:id", blogFinder, async (req, res) => {
-  if (req.blog) {
-    req.blog.likes = req.body.likes;
-    await req.blog.save();
-    res.json(req.blog);
-  } else {
-    res.status(404).end();
+  try {
+    if (req.blog) {
+      req.blog.likes = req.body.likes;
+      await req.blog.save();
+      res.json(req.blog);
+    } else {
+      res.status(404).end();
+    }
+  } catch (error) {
+    next(error);
   }
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+router.use(unknownEndpoint);
+
+router.use(errorHandler);
+
 module.exports = router;
