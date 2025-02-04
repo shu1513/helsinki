f641157cd62eef645e2af6793d08099aac11ebae
Shu Yang <shu@MacBookPro.lan>
2025-02-03 17:19:01 -0800

    13.5 & 13.6

---
+++ part13/blog-homework/controllers/blogs.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+
+const { Blog } = require("../models");
+
+const blogFinder = async (req, _res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
+
+router.get("/", async (req, res) => {
+  const blogs = await Blog.findAll();
+  console.log(JSON.stringify(blogs));
+  res.json(blogs);
+});
+
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  const blog = await Blog.create(req.body);
+  res.json(blog);
+});
+
+router.get("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    res.json(req.blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.delete("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy();
+
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes;
+    await req.blog.save();
+    res.json(req.blog);
+  } else {
+    res.status(404).end();
+  }
+});
+module.exports = router;
