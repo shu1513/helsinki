dad3f4493a754c94631b1aa1bdec1deb638442e3
Shu Yang <shu@MacBookPro.lan>
2025-03-23 15:01:17 -0700

    testing github actions

--- part11/full-stack-open-pokedex/app.js
+++ part11/full-stack-open-pokedex/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-// get the port from env variable
+// get the port from env variable.
 const PORT = process.env.PORT || 5001;
 
 app.use(express.static("dist"));
